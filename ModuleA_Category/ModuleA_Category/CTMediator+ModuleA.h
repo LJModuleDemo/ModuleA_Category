05148c923d742691e234e6499ebbb0084d172d2f
Lincoln Jiao <jiaopeng@163.com>
2017-10-18 17:35:21 +0800

    runnable 1.0.2

---
+++ ModuleA_Category/ModuleA_Category/CTMediator+ModuleA.h
@@ -0,0 +1,13 @@
+//
+//  CTMediator+ModuleA.h
+//  ModuleA_Category
+//
+//  Created by 焦鹏 on 2017/10/18.
+//  Copyright © 2017年 XES. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CTMediator_ModuleA : NSObject
+
+@end
---
+++ ModuleA_Category/ModuleA_Category/CTMediator+ModuleA.m
@@ -0,0 +1,13 @@
+//
+//  CTMediator+ModuleA.m
+//  ModuleA_Category
+//
+//  Created by 焦鹏 on 2017/10/18.
+//  Copyright © 2017年 XES. All rights reserved.
+//
+
+#import "CTMediator+ModuleA.h"
+
+@implementation CTMediator_ModuleA
+
+@end
